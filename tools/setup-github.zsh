289a9c54ec59c258303b3ed72b67a4b574dc3edf
fxstein <fxstein@users.noreply.github.com>
2020-01-29 20:50:22 +0000

    Improved config setup for github

--- tools/setup-github.zsh
+++ tools/setup-github.zsh
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env zsh
 
 # Basic script to set up the git config variables needed by GitHub.
@@ -7,23 +6,23 @@
 
 user=`git config --global user.name`
 email=`git config --global user.email`
+host=`hostname`
 
 # Setup github username
 if [ -z "$user" ]; then
-	read -p "GitHub committer name not found, please enter: " user
-    `git config --global user.name $user`
+	vared -p "GitHub committer name not found, please enter: " -c user
+	git config --global user.name $user
 fi
 
 # Setup email
 if [ -z "$email" ]; then
-	read -p "GitHub committer email not found, please enter: " email
-    `git config --global user.email $email`
+	vared -p "GitHub committer email not found, please enter: " -c email
+	git config --global user.email $email
 fi
 
 # SSH keys!
-if [ ! -f ~/.ssh/github-$user ]; then
-	read -p "No github-$user key found, generate one? (y/n) " reply
-	echo $reply
+if [ ! -f ~/.ssh/id_rsa_github-$host-$user ]; then
+	vared -p "No id_rsa_github-$host-$user key found, generate one? (y/n) " -c reply
 	if [ "$reply" != "${reply#[Yy]}" ] ;then
   		echo ""
 		echo $fg[red]"****************************************************************************"
@@ -32,23 +31,47 @@ if [ ! -f ~/.ssh/github-$user ]; then
 		echo $fg[red]"* After creating the key we will automatically add it to ssh-add for you   *"
 		echo $fg[red]"****************************************************************************"
 		echo ""
-		ssh-keygen -t rsa -b 4096 -C "$email" -f ~/.ssh/github-$user
+		ssh-keygen -t rsa -b 4096 -C "$email" -f ~/.ssh/id_rsa_github-$host-$user
 		# Make sure we have the proper permissions on the key files
-		chmod 600 ~/.ssh/github-$user
-		chmod 640 ~/.ssh/github-$user.pub
+		chmod 400 ~/.ssh/id_rsa_github-$host-$user
+		chmod 440 ~/.ssh/id_rsa_github-$host-$user.pub
 	fi
 fi
 
-# Add the passphrase to ssh-add
-if [ -f ~/.ssh/github-$user ]; then
-	ssh-add ~/.ssh/github-$user
+# Add github key to git configuration
+if [ -f ~/.ssh/id_rsa_github-$host-$user ]; then
+	if grep -qxF 'host github.com' ~/.ssh/config; then
+		# Have to potentially update existing setting
+		echo "Existing ssh config settings found..."
+
+		# TODO: Figure out if we should do something in this case...
+		# sed -e "/host github.com/,/ User git/{ /host github.com/ HostName github.com/ IdentityFile ~/.ssh/id_rsa_github-$host-$user/ User git/p; d }" ~/.ssh/config
+	else
+		# Create new ssh config settings
+		echo "#### BEGIN Auto Generated Github key for $host-$user ####" >> ~/.ssh/config
+		echo "host github.com" >> ~/.ssh/config
+		echo " HostName github.com" >> ~/.ssh/config
+		echo " IdentityFile ~/.ssh/id_rsa_github-$host-$user" >> ~/.ssh/config
+		echo " User git" >> ~/.ssh/config
+		echo "#### END Auto Generated Github key for $host-$user ####" >> ~/.ssh/config
+		# Make sure we have the right permissions
+		chmod 600 ~/.ssh/config
+	fi 
 fi
 
+# Add the passphrase to ssh-add
+#if [ -f ~/.ssh/id_rsa_github-$host-$user ]; then
+#	ssh-add ~/.ssh/id_rsa_github-$host-$user
+#fi
+
 # Add the key to github manually
-if [ -f ~/.ssh/github-$user ]; then
+if [ -f ~/.ssh/id_rsa_github-$host-$user.pub ]; then
 	echo ""
 	echo $fg[green] "Add this public key to your github keys":
 	echo ""
-	cat ~/.ssh/github-$user.pub
+	echo $fg[green]"Suggested Github keyname: github-$host-$user"
 	echo ""
+	cat ~/.ssh/id_rsa_github-$host-$user.pub
+	echo ""
+	echo "Add key here: https://github.com/settings/keys"
 fi
