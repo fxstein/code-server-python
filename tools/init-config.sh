1dac2bd76a16c2657e61a3bbe73d06d06aac0424
fxstein <fxstein@users.noreply.github.com>
2020-02-03 05:52:44 +0000

    Update init logging

--- tools/init-config.sh
+++ tools/init-config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "info Checking existing config setup."
+echo "info  Checking existing config setup"
 
 # Make sure we initialize the config inside of a potentially new volume
 if [ ! -f /home/coder/.config/.gitconfig ]; then
-    echo "info New install - setting up config."
+    echo "info    - New install - setting up config"
     # Make sure we own the files and directories
     chown coder:coder /home/coder/.config
     chown coder:coder /home/coder/project
@@ -19,7 +19,7 @@ if [ ! -f /home/coder/.config/.gitconfig ]; then
     chmod 640 /home/coder/.config/.gitconfig
     chmod 740 /home/coder/.config/.ssh
 
-    echo "info New install - config setup completed."
+    echo "info    - New install - config setup completed"
 else
-    echo "info Existing install - nothing to do."
+    echo "info  Existing install - nothing to init"
 fi
